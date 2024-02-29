bc1394771dac5e66c8d3da7ecef4a7e72d3f8d4d
shafi3000 <shafiulla.attar@prakalpana.com>
2024-02-29 17:41:51 +0530

    day 8

---
+++ Day 8/class-based/src/components/ErrorBoundary.js
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor() {
+    super();
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error) {
+    this.state({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <p>Something went wrong!</p>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
